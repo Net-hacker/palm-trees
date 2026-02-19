81049a9fa41f739f8780bbfb717398a294a28e5f
Nethack <85522443+Net-hacker@users.noreply.github.com>
2026-02-19 03:05:05 +0100

    Final changes before functionality
    Yeah that should be it i guess

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # palm-trees
 A "social media" platform for short messages like twitter.
+
+> [!Warning]
+> It's all just a work in progress programm of mine.
+> It's not functional as of writing this.
+> Thank You!
+
+## Requirements
+
+* Python
+* Flask
---
+++ python/static/script.js
@@ -0,0 +1,32 @@
+let mode = false;
+
+window.addEventListener('load', () => {
+  const m = sessionStorage.getItem('mode');
+  if (m === 'light') {
+    document.body.classList.toggle("light-mode");
+    mode = true;
+  }
+});
+
+function changeMode() {
+  if (mode === false) {
+    document.body.classList.toggle("light-mode");
+    sessionStorage.setItem('mode', 'light');
+    mode = true;
+  } else {
+    document.body.classList.toggle("light-mode");
+    sessionStorage.setItem('mode', 'dark');
+    mode = false;
+  }
+}
+
+function showMenu() {
+  const menu = document.getElementById("menu");
+  if (menu.hasAttribute('hidden')) {
+    menu.hidden = false;
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+    menu.hidden = true;
+  }
+}
